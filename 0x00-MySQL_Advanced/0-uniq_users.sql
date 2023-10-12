e73cbe1d7301bc65900469ce70af0228a7469e32
Ayom04 <abdullahayomide04@gmail.com>
2023-10-13 00:34:49 +0100

    started advanced sql projexct

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
---script that creates a table with the following fields:
+-- script that creates a table users with follwing fields
 -- id, email, name
-
-CREATE TABLE IF NOT EXISTS `users` (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE ,
-    name VARCHAR(255) 
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
 );
